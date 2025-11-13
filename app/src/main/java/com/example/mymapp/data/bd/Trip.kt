95da1642e6bb2cc1df72c8934f93cec9ff61be73
SanDaniel777 <20243ds156@utez.edu.mx>
2025-11-12 21:13:36 -0600

    se agrego todo lo de la capa data

---
+++ app/src/main/java/com/example/mymapp/data/bd/Trip.kt
@@ -0,0 +1,12 @@
+package com.example.mymapp.data.bd
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "trips")
+data class Trip(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val startTime: Long = System.currentTimeMillis(),
+    var endTime: Long? = null,
+    var photoUri: String? = null // Guardamos la URI de la foto como String
+)
